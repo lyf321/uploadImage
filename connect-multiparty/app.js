fa971d8371a627a5e4934a90d1a5bb7977ca426f
lyf <2531349593@qq.com>
2017-06-12 10:17:35 +0800

    add upload with connect-multiparty

--- connect-multiparty/app.js
+++ connect-multiparty/app.js
@@ -1,5 +1,5 @@
 let express = require("express");
-let multiparty = require("connect-multiparty");
+let multipart = require("connect-multiparty");
 let path = require("path");
 let fs = require("fs");
 
@@ -7,15 +7,15 @@ let app = express();
 
 app.use(express.static('./public'));
 
-app.post("/upload", function (req, res) {
+app.post("/upload", multipart(), function (req, res) {
     //get filename
     var filename = req.files.files.originalFilename || path.basename(req.files.files.ws.path);
     //copy file to a public directory
-    var targetPath = path.dirname(__filename) + '/public/upload' + filename;
+    var targetPath = path.dirname(__filename) + '/public/upload/' + filename;
     //copy file
     fs.createReadStream(req.files.files.ws.path).pipe(fs.createWriteStream(targetPath));
     //return file url
-    res.json({code: 200, msg: {url: 'http://' + req.headers.host + '/' + filename}});
+    res.json({code: 200, filepath: './upload/' + filename});
 });
 
 app.listen(3000);
--- connect-multiparty/public/multiparty.js
+++ connect-multiparty/public/multiparty.js
@@ -1,3 +1,28 @@
-/**
- * Created by liyangfan on 17-6-11.
- */
+function uploadImage() {
+    var formData = new FormData($("#frmUploadFile")[0]);
+
+    console.log(formData);
+
+    $.ajax({
+        url: '/upload',
+        type: 'POST',
+        data: formData,
+        async: false,
+        cache: false,
+        contentType: false,
+        processData: false,
+        success: function (data) {
+            if (200 === data.code) {
+                $("#imgShow").attr('src', data.filepath);
+                $("#spanMessage").html("上传成功");
+            } else {
+                $("#spanMessage").html("上传失败");
+            }
+            console.log('imgUploader upload success, data:', data);
+        },
+        error: function (err) {
+            console.log("err", err);
+            $("#spanMessage").html("与服务器通信发生错误");
+        }
+    });
+}
